5620f16a36065d019fb7c868cdcbee3ad9d001d1
redbeardanil <ahmetyaylalioglu@gmail.com>
2018-01-26 12:40:40 +0300

    Add files via upload
    Resize multiple images in a folder,without keeping aspect ratio!!And save with same name and order.Shortest way when compare with photoshop,pixresizer,photoscape (these apps always keep aspect ratio if working multiple files)

---
+++ resizeMulti.m
@@ -0,0 +1,10 @@
+fileFolder = fullfile('G:','CV for Faces','Dataset','MyFaceSemantic','V3','Train_RGB','aio');
+dirOutput = dir(fullfile(fileFolder,'*.bmp'));
+fileNames = {dirOutput.name};
+
+for k=1:length(fileNames)
+    H = fileNames{k};
+    S = imread(H);
+    I = imresize(S, [512 480]);
+    imwrite(I,fullfile('G:','CV for Faces','Dataset','MyFaceSemantic','V3','Train_RGB','aio','final',H));
+end
